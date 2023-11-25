28171869c2a4f5c26024220842ed77b55fe8c910
fleetcking <Fleet.king10@gmail.com>
2023-11-24 18:48:36 -0800

    added best of 3 game mode

--- Rock_Papper_Scissors.py
+++ Rock_Papper_Scissors.py
@@ -119,11 +119,74 @@ def cheats():
             player1_string.append("rock")
        
         print(f"{player1_name} chose {player1_string}, {player2_name} chose {player2_string}. {player1_name} Wins!")
-# 3 player multiplayer
+# best of 3
+def best_of_3():
+   
+    player1_score = int()
+    player2_score = int() 
+    
+    
+    #player 1 name
+    player1_name = input("Player 1, enter your name: ")
+    while player1_name == "":
+            print("you must type in your name: ")
+            player1_name = input("Player 1, enter your name: ")
+    #player 2 name
+    player2_name = input("player 2 enter your name: ")
+    while player2_name == "":
+            print("you must type in your name: ")
+            player2_name = input("Player 2, enter your name: ")
+    #game loop
+    while player1_score < 2 and player2_score < 2:
+        #player 1 guess
+        player1  = str.lower(input('''Player 1, guess rock paper scissors. type "r" for rock, "p" for paper, and "s" for scissors: '''))
+        while player1 != "r" and player1 != "p" and player1 != "s":
+            print(f"{player1} is invalid")
+            player1  = str.lower(input('''Try typeing "r" for rock, "p" for paper, and "s" for scissors: '''))
+        player1_string = []
+        if player1 == "r":
+            player1_string.append("rock")
+        if player1 == "p":
+            player1_string.append("paper")
+        if player1 == "s":
+            player1_string.append("scissors")
+        
+        #player 2 guess
+        player2  = str.lower(input('''Player 2, guess rock paper scissors. type "r" for rock, "p" for paper, and "s" for scissors: '''))
+        while player2 != "r" and player2 != "p" and player2 != "s":
+            print(f"{player2} is invalid")
+            player2  = str.lower(input('''Try typeing "r" for rock, "p" for paper, and "s" for scissors: '''))
+        player2_string = []
+        if player2 == "r":
+            player2_string.append("rock")
+        if player2 == "p":
+            player2_string.append("paper")
+        if player2 == "s":
+            player2_string.append("scissors")
+
+        #draw
+        if player1 == "r" and player2 == "r" or player1 == "p" and player2 == "p" or player1 == "s" and player2 == "s":
+            print(f"{player1_name} chose {player1_string}, {player2_name} chose {player2_string}. Thats a draw")
+        #player 1 win
+        if player1 == "r" and player2 == "s" or player1 == "p" and player2 == "r" or player1 == "s" and player2 == "p":
+            print(f"{player1_name} chose {player1_string}, {player2_name} chose {player2_string}. {player1_name} Wins!")
+            player1_score = player1_score + 1
+            
+            print(f"{player1_name} {player1_score} | {player2_name} {player2_score}")
+        #player 2 win
+        if player1 == "r" and player2 == "p" or player1 == "p" and player2 == "s" or player1 == "s" and player2 == "r":
+            print(f"{player1_name} chose {player1_string}, {player2_name} chose {player2_string}. {player2_name} Wins!")
+            player2_score = player2_score + 1
+            
+            print(f"{player1_name} {player1_score} | {player2_name} {player2_score}")
+    if player1_score > player2_score:
+        print(f"{player1_name} Wins!")
+    if player2_score > player1_score:
+        print(f"{player2_name} Wins!")
 
 #choosing single player or multiplayer 
-single_or_multi = str.lower(input('''singleplayer or multiplayer? "s" for single, m for multiplayer: '''))
-while single_or_multi != "s" and single_or_multi != "m" and single_or_multi != "c":
+single_or_multi = str.lower(input('''singleplayer or multiplayer? "s" for single, "m" for multiplayer, "3" for best of 3: '''))
+while single_or_multi != "s" and single_or_multi != "m" and single_or_multi != "c" and single_or_multi != "3":
     print(f"{single_or_multi} is invalid")
     single_or_multi = str.lower(input('''try typing "s" for single, m for multiplayer: '''))
 
@@ -139,8 +202,6 @@ if single_or_multi == "c":
     print("multiplayer game")
     cheats()
 
-#to do
-#add while loops on user inputs
-#add 3 player multiplayer
-#add a fun different game mode like maybe rock papper scissors bomb
-#add like best of 3 game mode 
+if single_or_multi == "3":
+    print("best of 3")
+    best_of_3()
