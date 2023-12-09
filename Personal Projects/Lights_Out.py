81010c3e1d0278cc1766e2530f7b70c187dea297
fleetcking <Fleet.king10@gmail.com>
2023-12-08 20:16:15 -0800

    Lights Out Update

--- Personal Projects/Lights_Out.py
+++ Personal Projects/Lights_Out.py
@@ -8,4 +8,24 @@
 #if a1 = true end game and if a2 = true end game ect
 #add together all squares and assign to a varyable. if variable == 25 end game and player wins. 
 #if variable does not == 25 when the time is out or if variable does not == 25 and the player runs out of turns, player looses.
+board = []
+import random
+ 
+for i in range(5):
+    # build each row
+    row = []
+    for j in range(5):
+        row.append(0)
+    board.append(row)
+ 
+print(board)
+ 
+ 
+ 
+random_row = random.randint(0, 4)
+random_col = random.randint(0, 4)
+ 
+board[random_row][random_col] = 1
+ 
+print(board)
 
